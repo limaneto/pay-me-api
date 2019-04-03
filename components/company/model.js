e7cb9d1dce1829a096fbcea1cb511410a15cd73d
lima neto <limanetocc@gmail.com>
2019-04-03 12:14:46 -0300

    Remove company related code

--- components/company/model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const BaseUserModel = require('../../config/baseUserSchema');
-
-const CompanySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  responsible: { type: String },
-});
-
-module.exports = BaseUserModel.discriminator('Company', CompanySchema);
