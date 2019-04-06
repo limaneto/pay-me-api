a5a1abceb2385225b27d0f4db8986b044b1c0f11
lima neto <limanetocc@gmail.com>
2019-04-05 23:41:05 -0300

    Change mongo server to Atlas

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_SERVER_PORT}/${process.env.DB_NAME}`, {
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.mongodb.net:${process.env.DB_PORT},debts-shard-00-01-2fzoq.mongodb.net:${process.env.DB_PORT},debts-shard-00-02-2fzoq.mongodb.net:${process.env.DB_PORT}/${process.env.DB_NAME}?ssl=true&replicaSet=Debts-shard-0&authSource=admin&retryWrites=true`, {
   connectTimeoutMS: 30000, keepAlive: 300000, promiseLibrary: global.Promise, useNewUrlParser: true,
 });
 
