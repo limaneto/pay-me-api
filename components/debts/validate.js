d3ed52935192e286f7cc632e112801e69083853a
lima neto <limanetocc@gmail.com>
2019-04-07 13:31:50 -0300

    Fix typo

--- components/debts/validate.js
+++ components/debts/validate.js
@@ -4,13 +4,13 @@ function hasCreditor(requestBody) {
 	return !!requestBody.creditor;
 }
 
-function hasDebitor(requestBody) {
+function hasDebtor(requestBody) {
 	return !!requestBody.debtor;
 }
 
 function addDebtValidator(req, res, next) {
 	const { body } = req;
-	if (!hasCreditor(body) && !hasDebitor(body)) {
+	if (!hasCreditor(body) && !hasDebtor(body)) {
 		const error = new ApiError('Pass at least a creditor or a debitor', 400);
 		return next(error);
 	}
