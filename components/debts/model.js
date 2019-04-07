c6030aebe24c87e3e909f573aec2a6d2d69dcc97
lima neto <limanetocc@gmail.com>
2019-04-07 20:16:53 -0300

    Add new model fields

--- components/debts/model.js
+++ components/debts/model.js
@@ -4,13 +4,16 @@ const mongoose = require('mongoose');
 const DebtSchema = new mongoose.Schema({
 	debtor: { type: mongoose.Schema.Types.ObjectId, ref: 'CustomUser' },
 	creditor: { type: mongoose.Schema.Types.ObjectId, ref: 'CustomUser' },
-	title: { type: String, minlength: 10, maxlength: 100, trim: true, required: false },
+	debtAccepted: { type: Boolean, default: false },
+	creditAccepted: { type: Boolean, default: false },
+	title: { type: String, minlength: 10, maxlength: 100, trim: true, required: true },
 	description: { type: String, minlength: 20, maxlength: 700, trim: true, required: false },
 	valueInitial: { type: Number, required: true },
 	valuePaid: { type: Number, required: false },
 	dateInitial: { type: Date, default: Date.now() },
 	dateDue: { type: Date },
 	datePaymentCompleted: { type: Date },
+	accepted: { type: Boolean, default: false },
 	partialPayments: [
 		{ date: { type: Date }, value: { type: Number } },
 	],
