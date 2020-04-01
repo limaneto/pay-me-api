d1025b9ae70ff2649a7a1d86be2f2ab87e90b278
lima neto <limanetocc@gmail.com>
2020-04-01 17:16:03 -0300

    Remove old dabatase folder

--- database/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-require('dotenv').config();
-
-mongoose.connect('mongodb://limaneto:Csf864791@ds119161.mlab.com:19161/payme', {
-	connectTimeoutMS: 30000, keepAlive: 300000, promiseLibrary: global.Promise, useNewUrlParser: true,
-});
-
-module.exports = mongoose;
