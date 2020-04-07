6d11a43cf21004d1204e62914f0f29e864f64d16
lima neto <limanetocc@gmail.com>
2020-04-07 18:14:53 -0300

    Save sequelize files

--- models/index.js
+++ models/index.js
@@ -1,34 +1,35 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/database.json')[env];
-const db = {};
+require('dotenv').config();
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const db = {};
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASSWORD, {
+		host: process.env.DB_HOST,
+		dialect: 'postgres'
+	}
+);
+
+// file => (file.indexOf('.') !== 0) arquivo que tem um ponto
+// file !== path.basename(__filename) não é esse arquivo index.js
+// file.slice(-3) === '.js' é um arquivo javascript
+// sequelize.import(path.join(__dirname, file)) junta o path com o nome do arquivo
 
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+	.readdirSync(__dirname)
+	.filter(file => (file.indexOf('.') !== 0) && (file !== path.basename(__filename)) && (file.slice(-3) === '.js'))
+	.forEach((file) => {
+		const model = sequelize['import'](path.join(__dirname, file));
+		db[model.name] = model;
+	});
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+Object.keys(db).forEach((modelName) => {
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
 db.sequelize = sequelize;
