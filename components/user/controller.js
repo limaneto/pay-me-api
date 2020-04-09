3abfc82ea8d3a54a13c335051ab7b381e91ca9c6
lima neto <limanetocc@gmail.com>
2020-04-09 19:47:48 -0300

    Improve Code

--- components/user/controller.js
+++ components/user/controller.js
@@ -1,4 +1,6 @@
 const models = require('../../models');
+const { DATABASE_FIELDS, POLYGLOT } = require('../../utils/constants');
+const { generateMessage } = require('../../utils/helpers');
 
 const login = (req, res, next, polyglot) => {
 	const { email, password } = req.body;
@@ -9,44 +11,32 @@ const login = (req, res, next, polyglot) => {
 				const passwordCheck = await user.isPasswordValid(password);
 				if (passwordCheck) {
 					const newToken = user.generateJWT();
-					res.status(200).json({ message: polyglot.t('logged', { model: polyglot.t('user') }), token: newToken });
+					res.status(200).json({ message: generateMessage(polyglot, 'logged', 'user'), token: newToken });
 				} else {
-					res.status(400).json({ message: polyglot.t('password-wrong') });
+					res.status(400).json({
+						errors: {
+							fields: [{
+									key: DATABASE_FIELDS.PASSWORD,
+									message: generateMessage(polyglot, POLYGLOT.WRONG_PASSWORD),
+								}
+							]
+						}
+					});
 				}
 			} else {
-				res.status(400).json({ message: polyglot.t('path-not-found', { path: polyglot.t('user') }) });
+				res.status(400).json({ errors: { message: generateMessage(polyglot, 'not-found', 'user') } });
 			}
 		})
 		.catch(err => next(err));
 };
 
-const fieldRequiredError = (field) => {
-	return res.status(400).json({ errors: { message: polyglot.t('field-required', { field: polyglot.t(field) }) } });
-};
 
 const save = async (req, res, next, polyglot) => {
-	if (!req.body.password) {
-		return fieldRequiredError('password');
-	}
-	if (!req.body.firstName) {
-		return fieldRequiredError('firstName');
-	}
-	if (!req.body.email) {
-		return fieldRequiredError('email');
-	}
 	try {
 		const user = await models.User.create(req.body);
-		return res.send({ message: polyglot.t('registered', { model: polyglot.t('user') }), user: user.toAuthJSON() });
+		return res.send({ message: polyglot.t('registered', { field: polyglot.t('user') }), user: user.toAuthJSON() });
 	} catch (err) {
-			if (err.name === 'ValidationError') {
-				return res.status(400).json({ errors: err.errors });
-			}
-
-			if (err.name === 'MongoError' && err.code === 11000) {
-				return res.status(400).json({ message: `Email ${req.body.email} already being used.` });
-			}
-
-			return res.status(500).json({ message: polyglot.t('500') });
+		return next(err);
 	}
 };
 
