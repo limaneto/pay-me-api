b9f4fd8fec487c8d9c9250f069dd6babb1d8a3db
lima neto <limanetocc@gmail.com>
2020-04-09 09:16:14 -0300

    Adapt to sequelize orm

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fileSystem = require('fs');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const Polyglot = require('node-polyglot');
-const mongoose = require('./database/mongoose');
+const models = require('./models');
 const routes = require('./routes');
 const handleError = require('./utils/errorHandler');
 const auth = require('./config/auth');
@@ -22,20 +22,16 @@ app.set('port', process.env.PORT);
 const data = fileSystem.readFileSync('./locales/pt-br.json', 'utf8');
 const polyglot = new Polyglot({ phrases: JSON.parse(data) });
 
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-	console.log('Database connected'.blue);
-});
-
 app.get('/', (req, res) => {
 	res.send('I hit / path\n');
 });
 
-app.listen(app.get('port'), () => {
-	console.log(`I am alive on port ${app.get('port')}.\n`.green);
-});
+models.sequelize.sync()
+	.then(() => {
+		app.listen(app.get('port'), () => {
+			console.log(`I am alive on port ${app.get('port')}.\n`.green);
+		});
+	});
 
 routes(app, polyglot);
 handleError(app);
