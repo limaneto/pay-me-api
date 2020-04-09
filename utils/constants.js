0d84722cf7087595939bbe8040345f36ffa32612
lima neto <limanetocc@gmail.com>
2020-04-09 19:48:02 -0300

    Add more constants

--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,13 @@ module.exports = {
 		VALIDATION: 'Validation error',
 	},
 	POLYGLOT: {
+		WRONG_PASSWORD: 'wrong-password',
 		FIELD_INVALID: 'field-invalid',
 		FIELD_REQUIRED: 'field-required',
+	},
+	DATABASE_FIELDS: {
+		FIRST_NAME: 'firstName',
+		PASSWORD: 'password',
+		LAST_NAME: 'lastName',
 	}
 };
