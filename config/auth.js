ff0bc39473138b135b076378e30927345c6661d3
lima neto <limanetocc@gmail.com>
2020-04-09 09:15:22 -0300

    Fix sintax

--- config/auth.js
+++ config/auth.js
@@ -1,16 +1,19 @@
 const passport = require('passport');
 const { Strategy, ExtractJwt } = require('passport-jwt');
-const User = require('../components/user/model');
+const { User } = require('../models');
 
 const jwtFromRequest = ExtractJwt.fromHeader('authorization');
 const secretOrKey = process.env.AUTH_SECRET;
 
 const JWTStrategy = new Strategy({ jwtFromRequest, secretOrKey }, (payload, done) => {
 	User
-		.findById(payload.id, (err, user) => {
-			if (err) done(err, false);
+		.findByPk(payload.id)
+		.then(user => {
 			if (user) done(null, user);
 			else done(null, false);
+		})
+		.catch(err => {
+			if (err) done(err, false);
 		});
 });
 
