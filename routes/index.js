41fde80abc4c8b330c8c89f1157b061546f8e696
lima neto <limanetocc@gmail.com>
2020-04-19 16:31:23 -0300

    Add Loans component and pluralize Users component

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-const debt = require('../components/pays/route');
-const user = require('../components/user/route');
+const loans = require('../components/loans/route');
+const pays = require('../components/pays/route');
+const users = require('../components/users/route');
 const friends = require('../components/friends/route');
 
 module.exports = (app, polyglot) => {
-	debt(app, polyglot);
+	loans(app, polyglot);
+	pays(app, polyglot);
 	friends(app, polyglot);
-	user(app, polyglot);
+	users(app, polyglot);
 };
