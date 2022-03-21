cac2cd96e97a94fa8e3f9298f87fe31b69f1c0a6
lima neto <limanetocc@gmail.com>
2022-03-21 09:32:04 -0300

    update model relation name

--- models/Friend.js
+++ models/Friend.js
@@ -1,23 +1,21 @@
-module.exports = (sequelize, DataTypes) => {
-	return sequelize.define('Friend', {
-		id: {
-			type: DataTypes.UUID,
-			defaultValue: DataTypes.UUIDV4,
-			primaryKey: true,
+module.exports = (sequelize, DataTypes) => sequelize.define('Friend', {
+	id: {
+		type: DataTypes.UUID,
+		defaultValue: DataTypes.UUIDV4,
+		primaryKey: true,
+	},
+	userId: {
+		type: DataTypes.INTEGER,
+		references: {
+			model: 'Users',
+			key: 'id',
 		},
-		userId: {
-			type: DataTypes.INTEGER,
-			references: {
-				model: 'User',
-				key: 'id',
-			}
+	},
+	friendId: {
+		type: DataTypes.INTEGER,
+		references: {
+			model: 'Users',
+			key: 'id',
 		},
-		friendId: {
-			type: DataTypes.INTEGER,
-			references: {
-				model: 'User',
-				key: 'id',
-			}
-		}
-	});
-};
+	},
+});
