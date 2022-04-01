5aba52d32fc6f329066b8ae215ffc13372229a16
lima neto <limanetocc@gmail.com>
2022-04-01 16:41:46 -0300

    Refactor database envs

--- database/instance.js
+++ database/instance.js
@@ -2,20 +2,23 @@ import Sequelize from 'sequelize';
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD, {
-		host: process.env.DB_HOST,
-		dialect: 'postgres',
-	},
-);
+const sequelizeOptions = {
+	username: process.env.DB_USER,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_HOST_PORT,
+	dialect: 'postgres',
+};
+
+const sequelize = new Sequelize(sequelizeOptions);
 
 export default sequelize;
 
 // uncomment for TESTING CONNECTION
-// sequelize.authenticate().then(() => {
-// 	console.log('Connection has been established successfully.');
-// }).catch((e) => {
-// 	console.log(e);
-// });
+sequelize.authenticate().then(() => {
+	console.log('Connection has been established successfully.');
+	console.log(sequelizeOptions);
+}).catch((e) => {
+	console.log(e);
+});
