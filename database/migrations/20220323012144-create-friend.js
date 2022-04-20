17a957657a38c0b351535c2260bbf42e3e18c0ed
lima neto <limanetocc@gmail.com>
2022-04-20 18:07:41 -0300

    Remove id field

--- database/migrations/20220323012144-create-friend.js
+++ database/migrations/20220323012144-create-friend.js
@@ -1,11 +1,6 @@
 module.exports = {
 	async up(queryInterface, Sequelize) {
 		await queryInterface.createTable('Friends', {
-			id: {
-				type: Sequelize.DataTypes.UUID,
-				defaultValue: Sequelize.DataTypes.UUIDV4,
-				primaryKey: true,
-			},
 			userId: {
 				type: Sequelize.DataTypes.UUID,
 				references: {
