3bd674c496be9979806fdbfb56c0738a9736def9
lima neto <limanetocc@gmail.com>
2022-04-20 18:58:14 -0300

    Remove rule from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "rules": {
-    "no-console": 0,
-    "no-underscore-dangle": 0,
-    "consistent-return": 0,
-    "padded-blocks": 0,
-	  "no-tabs": 0,
-	  "indent": [2, "tab"]
-  },
-  "env": {
-    "node": true
-  },
-  "globals": {
-    "CONFIG": true
-  }
+	extends: 'airbnb-base',
+	rules: {
+		'no-console': 0,
+		'no-underscore-dangle': 0,
+		'consistent-return': 0,
+		'padded-blocks': 0,
+		'no-tabs': 0,
+		indent: [2, 'tab'],
+		'object-curly-newline': 0,
+	},
+	env: {
+		node: true,
+	},
+	globals: {
+		CONFIG: true,
+	},
 };
