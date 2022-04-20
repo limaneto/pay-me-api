b49eb491f640ce6d2872fc1545a62f613810b75f
lima neto <limanetocc@gmail.com>
2022-04-20 18:08:18 -0300

    Change id integer to uuid

--- models/Friend.js
+++ models/Friend.js
@@ -2,20 +2,17 @@ import Sequelize from 'sequelize';
 import sequelize from '../database/instance';
 
 const Friend = sequelize.define('Friend', {
-	id: {
+	userId: {
 		type: Sequelize.DataTypes.UUID,
 		defaultValue: Sequelize.DataTypes.UUIDV4,
-		primaryKey: true,
-	},
-	userId: {
-		type: Sequelize.DataTypes.INTEGER,
 		references: {
 			model: 'Users',
 			key: 'id',
 		},
 	},
 	friendId: {
-		type: Sequelize.DataTypes.INTEGER,
+		type: Sequelize.DataTypes.UUID,
+		defaultValue: Sequelize.DataTypes.UUIDV4,
 		references: {
 			model: 'Users',
 			key: 'id',
