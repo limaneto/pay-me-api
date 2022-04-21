424361103e5aa9b3611d515ab2e049e41606cc86
lima neto <limanetocc@gmail.com>
2022-04-21 15:07:00 -0300

    Add date resolver for scalar schema date

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import { applyMiddleware } from 'graphql-middleware';
 import { makeExecutableSchema } from '@graphql-tools/schema';
+import { GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
 import { ApolloServer } from 'apollo-server-express';
 import Polyglot from 'node-polyglot';
 import express from 'express';
@@ -29,6 +31,22 @@ app.use(jwt({
 const schema = makeExecutableSchema({
 	typeDefs,
 	resolvers: {
+		Date: new GraphQLScalarType({
+			name: 'Date',
+			description: 'Date custom scalar type',
+			parseValue(value) {
+				return new Date(value); // value from the client
+			},
+			serialize(value) {
+				return value.getTime(); // value sent to the client
+			},
+			parseLiteral(ast) {
+				if (ast.kind === Kind.INT) {
+					return parseInt(ast.value, 10); // ast value is always in string format
+				}
+				return null;
+			},
+		}),
 		LoginResponse: {
 			__resolveType({ __typeName }) {
 				return __typeName;
